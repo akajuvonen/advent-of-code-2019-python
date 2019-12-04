08fbb08f58198a02821ecd9d69dfb34f71a42358
Antti Juvonen <akajuvonen@users.noreply.github.com>
2019-12-04 22:32:18 +0900

    Add solution to day 1 B (take fuel mass into account) + test

--- advent_of_code_2019_python/day1.py
+++ advent_of_code_2019_python/day1.py
@@ -7,17 +7,32 @@ def read_input(filename: str) -> List[int]:
         return [int(line.rstrip('\n')) for line in f.readlines()]
 
 
-def calculate_fuel(weight: int) -> int:
-    return weight // 3 - 2
+def calculate_fuel(mass: int) -> int:
+    return max(mass // 3 - 2, 0)
+
+
+def calculate_total_fuel(mass: int) -> int:
+    total_fuel = 0
+    remaining_mass = mass
+    while True:
+        remaining_mass = calculate_fuel(remaining_mass)
+        assert remaining_mass >= 0
+        if remaining_mass == 0:
+            return total_fuel
+        total_fuel += remaining_mass
 
 
 @click.command()
 @click.option('--input-file', required=True, type=str,
               help='Path to file containing module weights separated by newlines')
 def main(input_file):
-    inputs = read_input(input_file)
-    total_fuel = sum([calculate_fuel(input) for input in inputs])
-    print(f'Total amount of fuel needed: {total_fuel}')
+    masses = read_input(input_file)
+
+    initial_fuel = sum([calculate_fuel(mass) for mass in masses])
+    print(f'Amount of fuel needed for modules: {initial_fuel}')
+
+    total_fuel = sum([calculate_total_fuel(mass)for mass in masses])
+    print(f'Amount of fuel needed for modules and fuel: {total_fuel}')
 
 
 if __name__ == '__main__':
--- test/test_day1.py
+++ test/test_day1.py
@@ -1,8 +1,13 @@
 import pytest
 
-from advent_of_code_2019_python.day1 import calculate_fuel
+from advent_of_code_2019_python.day1 import calculate_fuel, calculate_total_fuel
 
 
-@pytest.mark.parametrize('weight, fuel', [(12, 2), (14, 2), (1969, 654), (100756, 33583)])
-def test_calculate_fuel(weight, fuel):
-    assert calculate_fuel(weight) == fuel
+@pytest.mark.parametrize('mass, fuel', [(12, 2), (14, 2), (1969, 654), (100756, 33583)])
+def test_calculate_fuel(mass, fuel):
+    assert calculate_fuel(mass) == fuel
+
+
+@pytest.mark.parametrize('mass, total_fuel', [(14, 2), (1969, 966), (100756, 50346)])
+def test_calculate_total_fuel(mass, total_fuel):
+    assert calculate_total_fuel(mass) == total_fuel
