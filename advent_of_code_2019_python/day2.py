1b1bfa139d02d5a1c0f2982c526c838bed59a3f0
Antti Juvonen <akajuvonen@users.noreply.github.com>
2019-12-07 19:08:24 +0900

    Day 2 part 2 solution

--- advent_of_code_2019_python/day2.py
+++ advent_of_code_2019_python/day2.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Tuple
 import click
 
 
@@ -9,6 +9,8 @@ OPCODE_HALT = 99
 # How many steps to take to find the next opcode
 N_STEPS = 4
 
+EXPECTED_OUTPUT = 19690720
+
 
 def read_input(filename: str) -> List[int]:
     with open(filename) as f:
@@ -16,15 +18,17 @@ def read_input(filename: str) -> List[int]:
     return [int(x) for x in intcode]
 
 
-def compute(intcode: List[int]) -> List[int]:
+def compute(intcode: List[int], noun: int, verb: int) -> List[int]:
     new_intcode = intcode.copy()
+    # Initialize noun and verb positions (index 1 and 2)
+    new_intcode[1] = noun
+    new_intcode[2] = verb
 
     # TODO what if at the end of the list and no 99
     i = 0
     while True:
         opcode = new_intcode[i]
         if opcode == OPCODE_HALT:
-            print('Opcode 99 found, halting program')
             break
         first_index = new_intcode[i+1]
         second_index = new_intcode[i+2]
@@ -40,18 +44,27 @@ def compute(intcode: List[int]) -> List[int]:
     return new_intcode
 
 
+def find_noun_verb(intcode: List[int], expected_output: int) -> Tuple[int, int]:
+    max_value = 99
+    for noun in range(max_value + 1):
+        for verb in range(max_value + 1):
+            if compute(intcode, noun, verb)[0] == expected_output:
+                return noun, verb
+
+
 @click.command()
 @click.option('--input-file', required=True, type=str, default='input.txt', show_default=True,
               help='Path to file containing Intcode program (comma-separated list)')
 def main(input_file):
     intcode = read_input(input_file)
 
-    # Return the program to the 1202 program alarm state
-    intcode[1] = 12
-    intcode[2] = 2
-
-    new_intcode = compute(intcode)
+    # Return the program to the 1202 program alarm state and get result
+    new_intcode = compute(intcode, 12, 2)
     print(new_intcode)
+
+    # Find inputs in positions 1 and 2 that produce the expected output
+    noun, verb = find_noun_verb(intcode, EXPECTED_OUTPUT)
+    print(f'Pair of inputs that produce output {EXPECTED_OUTPUT}: {noun} and {verb}')
 
 
 if __name__ == '__main__':
