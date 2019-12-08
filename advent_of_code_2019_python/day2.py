0e6081a7cebb67815b9acf825c5732b6ac0d4178
Antti Juvonen <akajuvonen@users.noreply.github.com>
2019-12-08 14:34:14 +0900

    DOCS: Add docstrings for day 2 functions.

--- advent_of_code_2019_python/day2.py
+++ advent_of_code_2019_python/day2.py
@@ -19,6 +19,21 @@ def read_input(filename: str) -> List[int]:
 
 
 def compute(intcode: List[int], noun: Optional[int] = None, verb: Optional[int] = None) -> List[int]:
+    """Computes  an intcode program result.
+    The intcode program contains an opcode (1, 2, or 99 for add, multiply, halt respectively)
+    and two input indices. The results after performing specified operation is saved to
+    output index. The next opcode is always 4 steps after the previous one.
+    Check https://adventofcode.com/2019/day/2 for details on what it's supposed to do.
+
+    Arguments:
+        intcode: List of integers containing opcodes, two input positions and
+            output positions to save the result to.
+        noun: An integer at index 1, known as noun. Affects the final results.
+        verg: An integer at index 2, known as verb.
+
+    Returns:
+        Final intcode program after performing all the operations.
+    """
     new_intcode = intcode.copy()
     # Initialize noun and verb positions (index 1 and 2)
     if noun is not None:
@@ -49,6 +64,17 @@ def compute(intcode: List[int], noun: Optional[int] = None, verb: Optional[int] 
 
 
 def find_noun_verb(intcode: List[int], expected_output: int) -> Optional[Tuple[int, int]]:
+    """Finds integers at intcode indices 1 and 2 that produce the expected output
+    on a given intcode program.
+
+    Arguments:
+        intcode: Intcode program as a list.
+        expected_output: The expected output integer that should be stored at index 0
+            after executing the program.
+
+    Returns:
+        Integers for positions 1 and 2 that produce the expected output.
+    """
     max_value = 99
     for noun in range(max_value + 1):
         for verb in range(max_value + 1):
@@ -66,6 +92,7 @@ def main(input_file):
     # Return the program to the 1202 program alarm state and get result
     new_intcode = compute(intcode, 12, 2)
     print(new_intcode)
+    print(f'Program output at index 0: {new_intcode[0]}')
 
     # Find inputs in positions 1 and 2 that produce the expected output
     noun, verb = find_noun_verb(intcode, EXPECTED_OUTPUT)
