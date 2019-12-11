49234595fe8c8cebf6df23f0c5cf997ac92a831a
Antti Juvonen <akajuvonen@users.noreply.github.com>
2019-12-11 22:25:19 +0900

    ENH: make code more reusable for part 2 + update test

--- advent_of_code_2019_python/day3.py
+++ advent_of_code_2019_python/day3.py
@@ -1,5 +1,5 @@
 import click
-from typing import List, Tuple, Set
+from typing import List, Tuple, Generator, Set
 
 
 def read_input(filename: str) -> Tuple[List[str], List[str]]:
@@ -8,8 +8,7 @@ def read_input(filename: str) -> Tuple[List[str], List[str]]:
     return wire_a.split(','), wire_b.split(',')
 
 
-def wire_path_to_set(paths: List[str]) -> Set[Tuple[int, int]]:
-    points = set()
+def calculate_path(paths: List[str]) -> Generator[Tuple[int, int], None, None]:
     xy = (0, 0)
     for path in paths:
         direction = path[0]
@@ -26,22 +25,16 @@ def wire_path_to_set(paths: List[str]) -> Set[Tuple[int, int]]:
             raise ValueError('Unexpected direction {direction}')
         for i in range(steps):
             xy = (xy[0] + movement[0], xy[1] + movement[1])
-            points.add(xy)
-    return points
+            yield xy
 
 
 def manhattan_distance_from_origin(x: int, y: int) -> int:
     return abs(x) + abs(y)
 
 
-def intersection_distances(wire_a_paths, wire_b_paths):
-    wire_a_points = wire_path_to_set(wire_a_paths)
-    wire_b_points = wire_path_to_set(wire_b_paths)
-
-    intersections = wire_a_points.intersection(wire_b_points)
-
-    return [manhattan_distance_from_origin(intersection[0], intersection[1]) for intersection in intersections]
-
+def min_distance(wire_a_points, wire_b_points):
+    intersections = set(wire_a_points).intersection(set(wire_b_points))
+    return min([manhattan_distance_from_origin(intersection[0], intersection[1]) for intersection in intersections])
 
 
 @click.command()
@@ -49,9 +42,11 @@ def intersection_distances(wire_a_paths, wire_b_paths):
               help='Path to file containing wire paths (AoC Day3 input.txt)')
 def main(input_file):
     wire_a_paths, wire_b_paths = read_input(input_file)
+    wire_a_points = [path for path in calculate_path(wire_a_paths)]
+    wire_b_points = [path for path in calculate_path(wire_b_paths)]
 
-    distances = intersection_distances(wire_a_paths, wire_b_paths)
-    print(f'Minimum intersection distance: {min(distances)}')
+    distance = min_distance(wire_a_points, wire_b_points)
+    print(f'Minimum intersection distance: {distance}')
 
 
 if __name__ == '__main__':
--- test/test_day3.py
+++ test/test_day3.py
@@ -1,12 +1,15 @@
 import pytest
-from advent_of_code_2019_python.day3 import intersection_distances
+from advent_of_code_2019_python.day3 import calculate_path, min_distance
 
 
-@pytest.mark.parametrize('paths_a, paths_b, min_distance',
+@pytest.mark.parametrize('paths_a, paths_b, expected_distance',
                          [(['R8', 'U5', 'L5', 'D3'], ['U7', 'R6', 'D4', 'L4'], 6),
                           (['R75', 'D30', 'R83', 'U83', 'L12', 'D49', 'R71', 'U7', 'L72'],
                            ['U62', 'R66', 'U55', 'R34', 'D71', 'R55', 'D58', 'R83'], 159),
                           (['R98', 'U47', 'R26', 'D63', 'R33', 'U87', 'L62', 'D20', 'R33', 'U53', 'R51'],
                            ['U98', 'R91', 'D20', 'R16', 'D67', 'R40', 'U7', 'R15', 'U6', 'R7'], 135)])
-def test_minimum_distance(paths_a, paths_b, min_distance):
-    assert min(intersection_distances(paths_a, paths_b)) == min_distance
+def test_minimum_distance(paths_a, paths_b, expected_distance):
+    points_a = [path for path in calculate_path(paths_a)]
+    points_b = [path for path in calculate_path(paths_b)]
+    distance = min_distance(points_a, points_b)
+    assert distance == expected_distance
