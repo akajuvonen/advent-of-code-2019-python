692bac935ece02adb12ded22b3da56d8771e62da
Antti Juvonen <akajuvonen@users.noreply.github.com>
2019-12-29 18:53:43 +0900

    Refactor things to avoid code repetition

--- advent_of_code_2019_python/intcode_computer.py
+++ advent_of_code_2019_python/intcode_computer.py
@@ -13,7 +13,9 @@ OPCODE_HALT = 99
 @attr.s
 class IntcodeComputer:
     intcode: List[int] = attr.ib()
-    orig_intcode = attr.ib(init=False)
+    orig_intcode: List[int] = attr.ib(init=False)
+    instruction: int = attr.ib(init=False)
+    instr_pointer: int = attr.ib(init=False, default=0)
 
     @orig_intcode.default
     def _init_orig_intcode(self):
@@ -25,13 +27,10 @@ class IntcodeComputer:
             intcode = f.read().rstrip('\n').split(',')
         return cls([int(x) for x in intcode])
 
-    def compute(self, noun: Optional[int] = None, verb: Optional[int] = None,
-                input_value: int = 1):
+    def compute(self, noun: Optional[int] = None, verb: Optional[int] = None, input_value: int = 1):
         """Computes an intcode program result.
 
         Arguments:
-            intcode: List of integers containing opcodes, two input positions and
-                output positions to save the result to.
             noun: An integer at index 1, known as noun. Affects the final results.
             verb: An integer at index 2, known as verb.
             input_value: An integer given as input to the program.
@@ -43,52 +42,55 @@ class IntcodeComputer:
             self.intcode[2] = verb
 
         try:
-            i = 0
             while True:
                 # The last two digits of the instruction
-                instruction = self.intcode[i]
-                opcode = instruction % 100
-                instruction //= 100
+                self.instruction = self.intcode[self.instr_pointer]
+                opcode = self.instruction % 100
+                self.instruction //= 100
                 if opcode == OPCODE_HALT:
                     break
                 n_steps = 4
                 if opcode == OPCODE_ADD:
-                    first_param_mode, instruction = _pop_last_digit(instruction)
-                    second_param_mode, instruction = _pop_last_digit(instruction)
-                    first_value = self.intcode[i+1] if first_param_mode else self.intcode[self.intcode[i+1]]
-                    second_value = self.intcode[i+2] if second_param_mode else self.intcode[self.intcode[i+2]]
-                    output_index = self.intcode[i+3]
-                    self.intcode[output_index] = first_value + second_value
+                    self._add_or_multiply()
                 elif opcode == OPCODE_MULTIPLY:
-                    first_param_mode, instruction = _pop_last_digit(instruction)
-                    second_param_mode, instruction = _pop_last_digit(instruction)
-                    first_value = self.intcode[i+1] if first_param_mode else self.intcode[self.intcode[i+1]]
-                    second_value = self.intcode[i+2] if second_param_mode else self.intcode[self.intcode[i+2]]
-                    output_index = self.intcode[i+3]
-                    self.intcode[output_index] = first_value * second_value
+                    self._add_or_multiply(multiply=True)
                 elif opcode == OPCODE_INPUT:
-                    self.intcode[self.intcode[i+1]] = input_value
+                    self.intcode[self.intcode[self.instr_pointer+1]] = input_value
                     n_steps = 2
                 elif opcode == OPCODE_OUTPUT:
-                    param_mode, instruction = _pop_last_digit(instruction)
-                    output_value = self.intcode[i+1] if param_mode else self.intcode[self.intcode[i+1]]
+                    param_mode = self._pop_last_digit()
+                    output_value = self.intcode[self.instr_pointer+1] if param_mode else self.intcode[self.intcode[
+                        self.instr_pointer+1]]
                     print(output_value)
                     n_steps = 2
                 else:
                     raise ValueError(f'Opcode {opcode} not supported')
-                i += n_steps
+                self.instr_pointer += n_steps
         except IndexError:
             print(f'Intcode index out of range, no instruction {OPCODE_HALT} found, stopping')
 
+    def _add_or_multiply(self, multiply=False):
+        first_param_mode = self._pop_last_digit()
+        second_param_mode = self._pop_last_digit()
+        first_value = self.intcode[self.instr_pointer + 1] if first_param_mode else self.intcode[
+            self.intcode[self.instr_pointer + 1]]
+        second_value = self.intcode[self.instr_pointer + 2] if second_param_mode else self.intcode[
+            self.intcode[self.instr_pointer + 2]]
+        output_index = self.intcode[self.instr_pointer + 3]
+        if multiply:
+            self.intcode[output_index] = first_value * second_value
+        else:
+            self.intcode[output_index] = first_value + second_value
+
     def reset(self):
         self.intcode = self.orig_intcode.copy()
+        self.instr_pointer = 0
 
     @property
     def output(self):
         return self.intcode[0]
 
-
-def _pop_last_digit(instruction):
-    digit = instruction % 10
-    instruction //= 10
-    return digit, instruction
+    def _pop_last_digit(self):
+        digit = self.instruction % 10
+        self.instruction //= 10
+        return digit
