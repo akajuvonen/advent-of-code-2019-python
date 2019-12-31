107da8f26a58146929506773a285d6855c058555
Antti Juvonen <akajuvonen@users.noreply.github.com>
2019-12-31 17:01:19 +0900

    Add day 6 part 1 solution

--- advent_of_code_2019_python/day6.py
+++ advent_of_code_2019_python/day6.py
@@ -1,7 +1,6 @@
 from __future__ import annotations
 
-from collections import defaultdict
-from typing import List, DefaultDict
+from typing import List, Dict
 
 import attr
 import click
@@ -9,27 +8,58 @@ import click
 
 @attr.s(auto_attribs=True)
 class SpaceObject:
+    name: str
     orbiters: List[SpaceObject] = attr.Factory(list)
 
     def add_orbiter(self, orbiter: SpaceObject):
         self.orbiters.append(orbiter)
 
 
-def populate_orbits(filename: str) -> DefaultDict[str, SpaceObject]:
-    all_objects: DefaultDict = defaultdict(SpaceObject)
-    with open(filename) as f:
-        for line in f:
-            [center_of_mass, orbiter] = line.rstrip('\n').split(')')
-            all_objects[center_of_mass].add_orbiter(orbiter)
-    print(all_objects)
-    return all_objects
+def populate_orbits(orbit_list: List[str]) -> SpaceObject:
+    orbit_dict = {}
+    for orbit in orbit_list:
+        [center_of_mass, orbiter] = orbit.split(')')
+        if center_of_mass not in orbit_dict:
+            orbit_dict[center_of_mass] = []
+        orbit_dict[center_of_mass].append(orbiter)
+    univ_center_of_mass = SpaceObject(name='COM')
+    _add_orbiters(univ_center_of_mass, orbit_dict)
+    return univ_center_of_mass
+
+
+def _add_orbiters(space_object: SpaceObject, orbit_dict: Dict[str, list]):
+    if space_object.name not in orbit_dict:
+        return
+    for orbiter_name in orbit_dict[space_object.name]:
+        orbiter = SpaceObject(name=orbiter_name)
+        space_object.add_orbiter(orbiter)
+        _add_orbiters(orbiter, orbit_dict)
+
+
+def calculate_orbits(univ_center_of_mass: SpaceObject) -> int:
+    return _traverse_objects(univ_center_of_mass, 0)
+
+
+def _traverse_objects(space_object: SpaceObject, count: int) -> int:
+    if not space_object.orbiters:
+        return count
+    total = 0
+    for orbiter in space_object.orbiters:
+        total += _traverse_objects(orbiter, count+1)
+    return total + count
 
 
 @click.command()
 @click.option('--input-file', required=True, type=str, default='inputs/input_day6.txt', show_default=True,
-              help='Path to file containing Intcode program (comma-separated list)')
+              help='Path to file containing space object orbits')
 def main(input_file):
-    populate_orbits(input_file)
+    orbit_list = []
+    with open(input_file) as f:
+        for line in f:
+            orbit_list.append(line.rstrip('\n'))
+    univ_center_of_mass = populate_orbits(orbit_list)
+    orbit_count = calculate_orbits(univ_center_of_mass)
+    print(orbit_count)
 
 
 if __name__ == '__main__':
---
+++ test/test_day6.py
@@ -0,0 +1,7 @@
+from advent_of_code_2019_python.day6 import populate_orbits, calculate_orbits
+
+
+def test_calculate_orbits():
+    orbit_list = ['COM)B', 'B)C', 'C)D', 'D)E', 'E)F', 'B)G', 'G)H', 'D)I', 'E)J', 'J)K', 'K)L']
+    univ_center_of_mass = populate_orbits(orbit_list)
+    assert calculate_orbits(univ_center_of_mass) == 42
