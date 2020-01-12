09ac4f094f5b25176ba54854cc3ce04309c58145
Antti Juvonen <akajuvonen@users.noreply.github.com>
2020-01-12 19:35:57 +0900

    STY: typing and docstrings

--- advent_of_code_2019_python/day8.py
+++ advent_of_code_2019_python/day8.py
@@ -1,11 +1,42 @@
 import click
-import numpy as np
+import numpy as np  # type: ignore
 
 
-def load_image(filename, width, height):
+def load_image(filename: str, width: int, height: int) -> np.ndarray:
+    """Load an image text file into numpy array with x layers.
+    Each layer has specified width and height.
+
+    Arguments:
+        filename: Filename to open.
+        width: width of each image layer
+        height: height of each layer
+
+    Returns:
+        Array with each layer separated.
+    """
     with open(filename) as f:
         image = np.array([int(x) for x in f.read().rstrip('\n')])
     return image.reshape((-1, height, width))
+
+
+def image_corruption_check(image: np.ndarray) -> int:
+    """Returns a checksum to see if an image is corrupted.
+    This is done by first finding the layer with fewest zeros.
+    Then, the number of ones is multiplied by number of twos, and this
+    value is returned.
+
+    Arguments:
+        image: Array with image pixel information on each layer
+
+    Returns:
+        Image checksum.
+    """
+    zeros_in_layers = np.array([layer.size - np.count_nonzero(layer) for layer in image])
+    fewest_zeros_layer_idx = np.argmin(zeros_in_layers)
+    fewest_zeros_layer = image[fewest_zeros_layer_idx]
+    ones = np.count_nonzero(fewest_zeros_layer == 1)
+    twos = np.count_nonzero(fewest_zeros_layer == 2)
+    return ones * twos
 
 
 @click.command()
@@ -16,13 +47,7 @@ def main(input_file):
     height = 6
     image = load_image(input_file, width, height)
 
-    zeros_in_layers = np.array([layer.size - np.count_nonzero(layer) for layer in image])
-    fewest_zeros_layer_idx = np.argmin(zeros_in_layers)
-    fewest_zeros_layer = image[fewest_zeros_layer_idx]
-    ones = np.count_nonzero(fewest_zeros_layer == 1)
-    twos = np.count_nonzero(fewest_zeros_layer == 2)
-    print("Number of digits 1 and 2 multiplied (in the layer with fewest 0 digits:)")
-    print(ones * twos)
+    print(image_corruption_check(image))
 
 
 if __name__ == '__main__':
