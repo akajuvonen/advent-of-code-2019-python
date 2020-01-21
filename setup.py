eca0548e68fbccdc1b2d37bf369ce27e217debc4
Antti Juvonen <akajuvonen@users.noreply.github.com>
2020-01-21 19:03:54 +0900

    CHORE: update version, .gitignore, add requirements

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup, find_packages
 
 setup(
     name='advent-of-code-2019-python',
-    version='0.8.0',
+    version='0.8.2',
     description='Advent of Code 2019 solutions in Python',
     author='Antti Juvonen',
     packages=find_packages(),
-    install_requires=['click', 'attr', 'numpy']
+    install_requires=['click', 'attrs', 'numpy']
 )
