e7f43b0927822412c9c4cf4944a5657b2de48ba1
Antti Juvonen <akajuvonen@users.noreply.github.com>
2020-01-25 16:53:27 +0900

    Day 9 part 2

--- advent_of_code_2019_python/day9.py
+++ advent_of_code_2019_python/day9.py
@@ -3,16 +3,24 @@ import click
 from advent_of_code_2019_python import IntcodeComputer
 
 
+def print_outputs(intcode_computer: IntcodeComputer):
+    intcode_computer.compute()
+    while not intcode_computer.halted:
+        print(intcode_computer.output)
+        intcode_computer.compute()
+
+
 @click.command()
 @click.option('--input-file', required=True, type=str, default='inputs/input_day9.txt', show_default=True,
               help='Path to file containing Intcode program (comma-separated list)')
 def main(input_file):
     intcode_computer = IntcodeComputer.from_file(input_file)
     intcode_computer.set_inputs(1)
-    intcode_computer.compute()
-    while not intcode_computer.halted:
-        print(intcode_computer.output)
-        intcode_computer.compute()
+    print_outputs(intcode_computer)
+
+    intcode_computer = IntcodeComputer.from_file(input_file)
+    intcode_computer.set_inputs(2)
+    print_outputs(intcode_computer)
 
 
 if __name__ == '__main__':
