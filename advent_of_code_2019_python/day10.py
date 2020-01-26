c2f32155797dcbfa98db9b0c3313f9f09b25f0c3
Antti Juvonen <akajuvonen@users.noreply.github.com>
2020-01-26 15:04:15 +0900

    FIX: visible asteroid calculation fixed

--- advent_of_code_2019_python/day10.py
+++ advent_of_code_2019_python/day10.py
@@ -1,32 +1,40 @@
-import click
 from typing import Set, Tuple
 
+import click
 
-def parse_asteroids(filename: str) -> Tuple[Set[Tuple[int, int]], int, int]:
+
+def parse_asteroids(filename: str) -> Tuple[Set[Tuple[int, int]], Tuple[int, int]]:
     asteroids = set()
     with open(filename) as f:
         for y, line in enumerate(f):
             for x, c in enumerate(line):
                 if c == '#':
                     asteroids.add((x, y))
-    return asteroids, x, y
+    return asteroids, (x, y)
 
 
 def calculate_visible_asteroids(all_asteroids: Set[Tuple[int, int]], location: Tuple[int, int],
                                 size: Tuple[int, int]) -> int:
+    # Copy to avoid changing the original set
     visible_asteroids = all_asteroids.copy()
     visible_asteroids.remove(location)
+    # Iterate over a copy and modify the original
     for asteroid in visible_asteroids.copy():
-        gcd = _gcd(*asteroid)
+        diff = tuple([b - a for a, b in zip(location, asteroid)])
+        gcd = _gcd(*diff)
+        diff = tuple([x // gcd for x in diff])
+        step = tuple([a + b for a, b in zip(location, diff)])
         first = True
-        location = tuple([coord // gcd for coord in asteroid])
-        while all([b < a for a, b in zip(size, location)]):
-            if location in visible_asteroids:
+        # While not going outside the asteroid field
+        while all([0 <= b < a for a, b in zip(size, step)]):
+            if step in visible_asteroids:
                 if first:
+                    # Never remove the first asteroid on line of sight
                     first = False
                 else:
-                    visible_asteroids.remove(location)
-            location = tuple([coord + gcd for coord in location])
+                    # Remove any asteroid blocked by the first one
+                    visible_asteroids.remove(step)
+            step = tuple([a + b for a, b in zip(step, diff)])
     return len(visible_asteroids)
 
 
@@ -34,7 +42,7 @@ def _gcd(a: int, b: int) -> int:
     """Calculate greatest common divisor."""
     while b:
         a, b = b, a % b
-    return a
+    return abs(a)
 
 
 @click.command()
