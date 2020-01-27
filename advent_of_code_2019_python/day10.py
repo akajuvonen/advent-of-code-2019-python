6c09938ad9742bcc5745db3d686efb67f69828e5
Antti Juvonen <akajuvonen@users.noreply.github.com>
2020-01-27 21:41:59 +0900

    Use a namedtuple for clarity

--- advent_of_code_2019_python/day10.py
+++ advent_of_code_2019_python/day10.py
@@ -1,46 +1,44 @@
+from collections import namedtuple
 from math import gcd
 from typing import Optional, Set, Tuple
 
 import click
 
 
-def parse_asteroids(filename: str) -> Tuple[Set[Tuple[int, int]], Tuple[int, int]]:
+Coord = namedtuple('Coord', ['x', 'y'])
+
+
+def parse_asteroids_with_max_size(filename: str) -> Tuple[Set[Coord], Coord]:
     asteroids = set()
     with open(filename) as f:
         for y, line in enumerate(f):
             for x, c in enumerate(line):
                 if c == '#':
-                    asteroids.add((x, y))
-    return asteroids, (x, y)
+                    asteroids.add(Coord(x, y))
+    return asteroids, Coord(x, y)
 
 
-def calculate_visible_asteroids(all_asteroids: Set[Tuple[int, int]], location: Tuple[int, int],
-                                size: Tuple[int, int]) -> int:
-    x, y = location
-    max_x, max_y = size
+def calculate_visible_asteroids(all_asteroids: Set[Coord], location: Coord, size: Coord) -> int:
     visible_asteroids = all_asteroids.copy()
     visible_asteroids.remove(location)
     for asteroid in visible_asteroids.copy():
-        asteroid_x, asteroid_y = asteroid
-        dx, dy = asteroid_x - x, asteroid_y - y
+        dx, dy = asteroid.x - location.x, asteroid.y - location.y
         divisor = gcd(dx, dy)
         dx //= divisor
         dy //= divisor
-        current_x, current_y = x + dx, y + dy
+        current_loc = Coord(location.x + dx, location.y + dy)
         first_asteroid_from_location = True
-        while 0 <= current_x < max_x and 0 <= current_y < max_y:
-            if (current_x, current_y) in visible_asteroids:
+        while 0 <= current_loc.x < size.x and 0 <= current_loc.y < size.y:
+            if current_loc in visible_asteroids:
                 if first_asteroid_from_location:
                     first_asteroid_from_location = False
                 else:
-                    visible_asteroids.remove((current_x, current_y))
-            current_x += dx
-            current_y += dy
+                    visible_asteroids.remove(current_loc)
+            current_loc = Coord(current_loc.x + dx, current_loc.y + dy)
     return len(visible_asteroids)
 
 
-def find_best_location(asteroids: Set[Tuple[int, int]], size: Tuple[int, int]) \
-        -> Tuple[Optional[Tuple[int, int]], int]:
+def find_best_location(asteroids: Set[Coord], size: Coord) -> Tuple[Optional[Coord], int]:
     max_visible_asteroids = 0
     best_location = None
     for asteroid in asteroids:
@@ -55,7 +53,7 @@ def find_best_location(asteroids: Set[Tuple[int, int]], size: Tuple[int, int]) \
 @click.option('--input-file', required=True, type=str, default='inputs/input_day10.txt', show_default=True,
               help='Path to file containing asteroid locations.')
 def main(input_file):
-    asteroids, size = parse_asteroids(input_file)
+    asteroids, size = parse_asteroids_with_max_size(input_file)
     best_loc, visible_asteroids = find_best_location(asteroids, size)
     print(f'Best location at {best_loc} with {visible_asteroids} visible asteroids')
 
--- test/test_day10.py
+++ test/test_day10.py
@@ -1,16 +1,17 @@
-from advent_of_code_2019_python.day10 import calculate_visible_asteroids, find_best_location
+from advent_of_code_2019_python.day10 import calculate_visible_asteroids, find_best_location, Coord
+
+
+ASTEROIDS = {Coord(1, 0), Coord(4, 0), Coord(0, 2), Coord(1, 2), Coord(2, 2), Coord(3, 2), Coord(4, 2), Coord(4, 3),
+             Coord(3, 4), Coord(4, 4)}
+LOCATION = Coord(3, 4)
+SIZE = Coord(5, 5)
 
 
 def test_calculate_visible_asteroids():
-    asteroids = {(1, 0), (4, 0), (0, 2), (1, 2), (2, 2), (3, 2), (4, 2), (4, 3), (3, 4), (4, 4)}
-    location = (3, 4)
-    size = (5, 5)
-    assert calculate_visible_asteroids(asteroids, location, size) == 8
+    assert calculate_visible_asteroids(ASTEROIDS, LOCATION, SIZE) == 8
 
 
 def test_find_best_location():
-    asteroids = {(1, 0), (4, 0), (0, 2), (1, 2), (2, 2), (3, 2), (4, 2), (4, 3), (3, 4), (4, 4)}
-    size = (5, 5)
-    location, visible_asteroids = find_best_location(asteroids, size)
-    assert location == (3, 4)
+    location, visible_asteroids = find_best_location(ASTEROIDS, SIZE)
+    assert location == Coord(3, 4)
     assert visible_asteroids == 8
