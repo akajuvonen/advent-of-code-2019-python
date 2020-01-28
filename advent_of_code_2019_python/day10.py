e24ed531fd2d70efa392208550bc8491769a6b0e
Antti Juvonen <akajuvonen@users.noreply.github.com>
2020-01-28 21:43:33 +0900

    Simplify visible asteroid calculation

--- advent_of_code_2019_python/day10.py
+++ advent_of_code_2019_python/day10.py
@@ -8,39 +8,31 @@ import click
 Coord = namedtuple('Coord', ['x', 'y'])
 
 
-def parse_asteroids_with_max_size(filename: str) -> Tuple[Set[Coord], Coord]:
+def parse_asteroids(filename: str) -> Set[Coord]:
     asteroids = set()
     with open(filename) as f:
         for y, line in enumerate(f):
             for x, c in enumerate(line):
                 if c == '#':
                     asteroids.add(Coord(x, y))
-    return asteroids, Coord(x, y)
+    return asteroids
 
 
-def calculate_visible_asteroids(all_asteroids: Set[Coord], location: Coord, size: Coord) -> int:
+def calculate_visible_asteroids(all_asteroids: Set[Coord], location: Coord) -> int:
     unique_line_of_sights = set()
     for asteroid in all_asteroids:
-        if asteroid == location:
-            continue
         dx, dy = asteroid.x - location.x, asteroid.y - location.y
         divisor = gcd(dx, dy)
-        dx //= divisor
-        dy //= divisor
-        current_loc = Coord(location.x + dx, location.y + dy)
-        while 0 <= current_loc.x < size.x and 0 <= current_loc.y < size.y:
-            if current_loc in all_asteroids:
-                unique_line_of_sights.add(Coord(dx, dy))
-                break
-            current_loc = Coord(current_loc.x + dx, current_loc.y + dy)
+        if divisor:
+            unique_line_of_sights.add((dx // divisor, dy // divisor))
     return len(unique_line_of_sights)
 
 
-def find_best_location(asteroids: Set[Coord], size: Coord) -> Tuple[Optional[Coord], int]:
+def find_best_location(asteroids: Set[Coord]) -> Tuple[Optional[Coord], int]:
     max_visible_asteroids = 0
     best_location = None
     for asteroid in asteroids:
-        visible_asteroids = calculate_visible_asteroids(asteroids, asteroid, size)
+        visible_asteroids = calculate_visible_asteroids(asteroids, asteroid)
         if visible_asteroids > max_visible_asteroids:
             max_visible_asteroids = visible_asteroids
             best_location = asteroid
@@ -51,8 +43,8 @@ def find_best_location(asteroids: Set[Coord], size: Coord) -> Tuple[Optional[Coo
 @click.option('--input-file', required=True, type=str, default='inputs/input_day10.txt', show_default=True,
               help='Path to file containing asteroid locations.')
 def main(input_file):
-    asteroids, size = parse_asteroids_with_max_size(input_file)
-    best_loc, visible_asteroids = find_best_location(asteroids, size)
+    asteroids = parse_asteroids(input_file)
+    best_loc, visible_asteroids = find_best_location(asteroids)
     print(f'Best location at {best_loc} with {visible_asteroids} visible asteroids')
 
 
--- test/test_day10.py
+++ test/test_day10.py
@@ -4,14 +4,13 @@ from advent_of_code_2019_python.day10 import calculate_visible_asteroids, find_b
 ASTEROIDS = {Coord(1, 0), Coord(4, 0), Coord(0, 2), Coord(1, 2), Coord(2, 2), Coord(3, 2), Coord(4, 2), Coord(4, 3),
              Coord(3, 4), Coord(4, 4)}
 LOCATION = Coord(3, 4)
-SIZE = Coord(5, 5)
 
 
 def test_calculate_visible_asteroids():
-    assert calculate_visible_asteroids(ASTEROIDS, LOCATION, SIZE) == 8
+    assert calculate_visible_asteroids(ASTEROIDS, LOCATION) == 8
 
 
 def test_find_best_location():
-    location, visible_asteroids = find_best_location(ASTEROIDS, SIZE)
+    location, visible_asteroids = find_best_location(ASTEROIDS)
     assert location == Coord(3, 4)
     assert visible_asteroids == 8
