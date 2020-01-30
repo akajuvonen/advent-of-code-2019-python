0c8d23d1c9ddedbde455e7ce3c0aca2d0339a5e0
Antti Juvonen <akajuvonen@users.noreply.github.com>
2020-01-30 19:09:05 +0900

    Use python 3.8

--- setup.py
+++ setup.py
@@ -7,5 +7,6 @@ setup(
     description='Advent of Code 2019 solutions in Python',
     author='Antti Juvonen',
     packages=find_packages(),
+    python_requires='>=3.8',
     install_requires=['click', 'attrs', 'numpy']
 )
