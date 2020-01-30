962f1ae1018339e06b4ce298614a665a4b7beffe
Antti Juvonen <akajuvonen@users.noreply.github.com>
2020-01-30 19:09:18 +0900

    part 2 solution

--- advent_of_code_2019_python/day10.py
+++ advent_of_code_2019_python/day10.py
@@ -1,9 +1,8 @@
 from collections import namedtuple
-from math import gcd
-from typing import Optional, Set, Tuple
+from math import atan2, degrees, gcd
+from typing import List, Optional, Set, Tuple
 
 import click
-
 
 Coord = namedtuple('Coord', ['x', 'y'])
 
@@ -18,25 +17,47 @@ def parse_asteroids(filename: str) -> Set[Coord]:
     return asteroids
 
 
-def calculate_visible_asteroids(all_asteroids: Set[Coord], location: Coord) -> int:
+def _calculate_unique_lines_of_sight(asteroids: Set[Coord], location: Coord) -> Set[Coord]:
+    """Calculates unique lines of sight, which means lines which directly lead to one or more asteroids."""
     unique_line_of_sights = set()
-    for asteroid in all_asteroids:
+    for asteroid in asteroids:
         dx, dy = asteroid.x - location.x, asteroid.y - location.y
         divisor = gcd(dx, dy)
         if divisor:
-            unique_line_of_sights.add((dx // divisor, dy // divisor))
-    return len(unique_line_of_sights)
+            unique_line_of_sights.add(Coord(dx // divisor, dy // divisor))
+    return unique_line_of_sights
 
 
 def find_best_location(asteroids: Set[Coord]) -> Tuple[Optional[Coord], int]:
+    """Find location with maximum visible asteroids."""
     max_visible_asteroids = 0
     best_location = None
     for asteroid in asteroids:
-        visible_asteroids = calculate_visible_asteroids(asteroids, asteroid)
+        visible_asteroids = len(_calculate_unique_lines_of_sight(asteroids, asteroid))
         if visible_asteroids > max_visible_asteroids:
             max_visible_asteroids = visible_asteroids
             best_location = asteroid
     return best_location, max_visible_asteroids
+
+
+def vaporize_asteroids(asteroids: Set[Coord], location: Coord) -> List[Coord]:
+    """Vaporize asteroids with a lazer turning 360 degrees multiple times. Visible asteroids will be destroyed
+    each rotation.
+    """
+    vaporized = []
+    remaining_asteroids = asteroids.copy()
+    remaining_asteroids.remove(location)
+    while remaining_asteroids:
+        loss = _calculate_unique_lines_of_sight(remaining_asteroids, location)
+        loss_sorted = sorted(loss, key=lambda coord: degrees(atan2(coord.x, -coord.y)) % 360)
+        for los in loss_sorted:
+            pos = location
+            while pos := Coord(pos.x + los.x, pos.y + los.y):
+                if pos in remaining_asteroids:
+                    remaining_asteroids.remove(pos)
+                    vaporized.append(pos)
+                    break
+    return vaporized
 
 
 @click.command()
@@ -46,6 +67,9 @@ def main(input_file):
     asteroids = parse_asteroids(input_file)
     best_loc, visible_asteroids = find_best_location(asteroids)
     print(f'Best location at {best_loc} with {visible_asteroids} visible asteroids')
+
+    vaporized = vaporize_asteroids(asteroids, best_loc)
+    print(vaporized[199])
 
 
 if __name__ == '__main__':
