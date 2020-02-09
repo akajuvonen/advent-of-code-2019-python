1554b6a9f9697ed0a180945215e78545adcd3c57
Antti Juvonen <akajuvonen@users.noreply.github.com>
2020-02-09 16:54:51 +0900

    Refactor panel painting into its own function

--- advent_of_code_2019_python/day11.py
+++ advent_of_code_2019_python/day11.py
@@ -1,11 +1,29 @@
 from collections import defaultdict, namedtuple
-from typing import Tuple
+from typing import Dict, Tuple
 
 import click
 
 from advent_of_code_2019_python import IntcodeComputer
 
 Position = namedtuple('Position', ['x', 'y'])
+
+
+def paint_panels(intcode_computer: IntcodeComputer, initial_color: int) -> Dict[Position, int]:
+    colors: Dict[Position, int] = defaultdict(int)
+    position = Position(x=0, y=0)
+    dx = 0
+    dy = 1
+    colors[position] = initial_color
+
+    while not intcode_computer.halted:
+        intcode_computer.set_inputs(colors[position])
+        color = intcode_computer.compute()
+        colors[position] = color
+        direction = intcode_computer.compute()
+        dx, dy = turn_right(dx, dy) if direction else turn_left(dx, dy)
+        position = Position(position.x + dx, position.y + dy)
+
+    return colors
 
 
 def turn_left(x: int, y: int) -> Tuple[int, int]:
@@ -21,22 +39,7 @@ def turn_right(x: int, y: int) -> Tuple[int, int]:
               help='Path to file containing Intcode program (comma-separated list)')
 def main(input_file):
     intcode_computer = IntcodeComputer.from_file(input_file)
-    colors = defaultdict(int)
-    position = Position(x=0, y=0)
-    dx = 0
-    dy = 1
-
-    painted_panels = set()
-
-    while not intcode_computer.halted:
-        intcode_computer.set_inputs(colors[position])
-        color = intcode_computer.compute()
-        colors[position] = color
-        painted_panels.add(position)
-        direction = intcode_computer.compute()
-        dx, dy = turn_right(dx, dy) if direction else turn_left(dx, dy)
-        position = Position(position.x + dx, position.y + dy)
-
+    painted_panels = paint_panels(intcode_computer, 0)
     print(f'Panels painted at least once: {len(painted_panels)}')
 
 
