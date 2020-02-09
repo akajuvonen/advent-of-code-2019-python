151b95016f4ea04b1205ca63cd393aac60ed8182
Antti Juvonen <akajuvonen@users.noreply.github.com>
2020-02-09 17:46:43 +0900

    day 11 part 2 solution

--- advent_of_code_2019_python/day11.py
+++ advent_of_code_2019_python/day11.py
@@ -4,6 +4,7 @@ from typing import Dict, Tuple
 import click
 
 from advent_of_code_2019_python import IntcodeComputer
+import numpy as np
 
 Position = namedtuple('Position', ['x', 'y'])
 
@@ -18,7 +19,8 @@ def paint_panels(intcode_computer: IntcodeComputer, initial_color: int) -> Dict[
     while not intcode_computer.halted:
         intcode_computer.set_inputs(colors[position])
         color = intcode_computer.compute()
-        colors[position] = color
+        if color is not None:
+            colors[position] = color
         direction = intcode_computer.compute()
         dx, dy = turn_right(dx, dy) if direction else turn_left(dx, dy)
         position = Position(position.x + dx, position.y + dy)
@@ -27,11 +29,26 @@ def paint_panels(intcode_computer: IntcodeComputer, initial_color: int) -> Dict[
 
 
 def turn_left(x: int, y: int) -> Tuple[int, int]:
+    """Rotate a vector counter clockwise."""
     return -y, x
 
 
 def turn_right(x: int, y: int) -> Tuple[int, int]:
+    """Rotate a vector clockwise."""
     return y, -x
+
+
+def generate_image(panels):
+    xs = []
+    ys = []
+    for panel in panels:
+        xs.append(panel.x)
+        ys.append(-panel.y)
+    image = np.zeros((max(ys) + 1, max(xs) + 1), dtype=np.int8)
+    for panel in panels:
+        image[-panel.y][panel.x] = panels[panel]
+    return image
+
 
 
 @click.command()
@@ -42,6 +59,10 @@ def main(input_file):
     painted_panels = paint_panels(intcode_computer, 0)
     print(f'Panels painted at least once: {len(painted_panels)}')
 
+    intcode_computer = IntcodeComputer.from_file(input_file)
+    painted_panels = paint_panels(intcode_computer, 1)
+    np.set_printoptions(formatter={'all': lambda x: '#' if x == 1 else '.'}, linewidth=100)
+    print(generate_image(painted_panels))
 
 if __name__ == '__main__':
     main()
