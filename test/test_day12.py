3c0f5343ffe7de1f416da6275587935f9eaf7db7
Antti Juvonen <akajuvonen@users.noreply.github.com>
2020-02-11 12:27:11 +0900

    TEST: More accurate test for gravity

--- test/test_day12.py
+++ test/test_day12.py
@@ -2,9 +2,7 @@ from advent_of_code_2019_python.day12 import apply_gravity, Moon, Coord
 
 
 def test_velocity():
-    moons = [Moon(Coord(0, 0, 0), velocity=Coord(0, 0, 0)), Moon(Coord(2, 2, 2), Coord(0, 0, 0))]
+    moons = [Moon(position=Coord(0, 0, 0), velocity=Coord(0, 0, 0)), Moon(position=Coord(2, 2, 2), velocity=Coord(0, 0, 0))]
+    expected = [Moon(Coord(1, 1, 1), Coord(1, 1, 1)), Moon(Coord(1, 1, 1), Coord(-1, -1, -1))]
     apply_gravity(moons, 1)
-    assert moons[0].position.x == 1
-    assert moons[0].velocity.x == 1
-    assert moons[1].position.x == 1
-    assert moons[1].velocity.x == -1
+    assert moons == expected
