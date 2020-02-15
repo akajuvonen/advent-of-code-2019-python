5402e8b9fc141bdb9743c3efa976aa71c4b02676
Antti Juvonen <akajuvonen@users.noreply.github.com>
2020-02-15 20:49:00 +0900

    Construct and print game screen

--- advent_of_code_2019_python/day13.py
+++ advent_of_code_2019_python/day13.py
@@ -2,7 +2,14 @@ import click
 
 from advent_of_code_2019_python import IntcodeComputer
 
+import numpy as np
 
+def _array_formatter(x):
+    icons = {1: '#',
+             2: 'X',
+             3: '_',
+             4: 'O'}
+    return icons[x] if x in icons else '.'
 
 
 @click.command()
@@ -15,9 +22,21 @@ def main(input_file):
         x = intcode_computer.compute()
         y = intcode_computer.compute()
         tile = intcode_computer.compute()
-        tiles[(x, y)] = tile
+        if x is not None and y is not None:
+            tiles[(x, y)] = tile
     print(f' Number of block tiles on the screen: {list(tiles.values()).count(2)}')
 
+    max_x, max_y = 0, 0
+    for x, y in tiles:
+        if x > max_x:
+            max_x = x
+        if y > max_y:
+            max_y = y
+    screen = np.zeros((max_y + 1, max_x + 1), dtype=np.int8) 
+    for x, y in tiles:
+        screen[y][x] = tiles[(x, y)]
+    np.set_printoptions(formatter={'all': _array_formatter}, linewidth=100)
+    print(screen)
 
 if __name__ == '__main__':
     main()
