cc627aca8bc8aa3656ac87ba21de60c0ba8f84cb
Antti Juvonen <akajuvonen@users.noreply.github.com>
2020-03-23 16:22:32 +0900

    Parse outputs into an array

--- advent_of_code_2019_python/day17.py
+++ advent_of_code_2019_python/day17.py
@@ -1,5 +1,7 @@
-from advent_of_code_2019_python.intcode_computer import IntcodeComputer
 import click
+import numpy as np
+
+from advent_of_code_2019_python.intcode_computer import IntcodeComputer
 
 
 @click.command()
@@ -7,11 +9,22 @@ import click
               help='Path to file containing Intcode program (comma-separated list)')
 def main(input_file):
     intcode_computer = IntcodeComputer.from_file(input_file)
+    scaffolds = []
+    i = 0
+    scaffolds.append([])
     output = intcode_computer.compute()
     while not intcode_computer.halted:
-        char = chr(output)
-        print(char, end='')
+        if output == 10:
+            scaffolds.append([])
+            i += 1
+        else:
+            scaffolds[i].append(output)
         output = intcode_computer.compute()
+    # The program outputs a couple of empty lines at the end, let's clear the resulting empty lists
+    while not scaffolds[-1]:
+        scaffolds.pop()
+    scaffolds_array = np.array(scaffolds)
+    print(scaffolds_array)
 
 
 if __name__ == '__main__':
